ce4c4dac2a17bb5bd432699ce31dd4f9b00e0e83
Shajedul Kabir Rafi <115880259+Rafi12234@users.noreply.github.com>
2026-05-16 23:21:45 +0600

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1964,7 +1964,7 @@ class _HowStep extends StatelessWidget {
           width: 52,
           height: 52,
           decoration: BoxDecoration(
-            color: color.withValues(alpha: 0.12),
+            color: color.withValues(alpha:0.12),
             shape: BoxShape.circle,
             border: Border.all(
                 color: color.withValues(alpha: 0.35), width: 1.5),
